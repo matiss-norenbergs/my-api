d05b86094cc427b6f2d6f97b69c07e1594d23a30
matiss-norenbergs <97153551+matiss-norenbergs@users.noreply.github.com>
2024-07-28 14:27:10 +0300

    first commit

---
+++ my-api/React/User/Components/UserListComp.cs
@@ -0,0 +1,26 @@
+﻿namespace my_api.React.User.Components
+{
+    public static class UserListComp
+    {
+        internal static object? HandleRequest(string operation, Dictionary<string, string> postParams)
+        {
+            object? result = null;
+            try
+            {
+                switch (operation)
+                {
+                    case "get":
+                        result = postParams;
+                        break;
+                    default:
+                        throw new Exception("Error, invalid operation!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ my-api/React/User/UserController.cs
@@ -0,0 +1,28 @@
+﻿using my_api.React.User.Components;
+
+namespace my_api.React.User
+{
+    public static class UserController
+    {
+        internal static object? HandleRequest(string type, string operation, Dictionary<string, string> postParams)
+        {
+            object? result = null;
+            try
+            {
+                switch (type)
+                {
+                    case "list":
+                        result = UserListComp.HandleRequest(operation, postParams);
+                        break;
+                    default:
+                        throw new Exception("Error, invalid user type!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    }
+}
