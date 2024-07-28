beccaf1157dd6d10fb32dc574d89b085051837af
matiss-norenbergs <97153551+matiss-norenbergs@users.noreply.github.com>
2024-07-28 15:15:38 +0300

    query

--- my-api/Controllers/CommonController.cs
+++ my-api/Controllers/CommonController.cs
@@ -5,11 +5,48 @@ using System.Text.Json;
 namespace my_api.Controllers
 {
     [ApiController]
-    [Route("api/react")]
+    [Route("api")]
     public class CommonController : ControllerBase
     {
         [HttpPost("{reactObject}/{type}/{operation}")]
         public async Task<ActionResult> HandleRequest(string reactObject, string type, string operation)
+        {
+            object? result = null;
+            try
+            {
+                using StreamReader reader = new(Request.Body, leaveOpen: false);
+                var bodyAsString = await reader.ReadToEndAsync();
+
+                var postParams = new Dictionary<string, string>();
+
+                if (!string.IsNullOrEmpty(bodyAsString))
+                {
+                    var test = JsonSerializer.Deserialize<Dictionary<string, object>>(bodyAsString);
+                    foreach (var item in test)
+                        postParams[item.Key] = item.Value.ToString() ?? string.Empty;
+                }
+
+                switch (reactObject)
+                {
+                    case "user":
+                        result = UserController.HandleRequest(type, operation, postParams);
+                        if (result == null)
+                            throw new Exception("Error getting data");
+                        break;
+                    default:
+                        throw new Exception("Error, invalid type");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest(ex.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> HandleRequest2([FromQuery] string reactObject, [FromQuery] string type, [FromQuery] string operation)
         {
             object? result = null;
             try
